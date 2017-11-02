3e4da1a4d3eb49b2b7c7a07423d998647d414ca7
ze25800000 <ze258100000@sina.com>
2017-11-02 11:53:57 +0800

    5-3 登录页面的开发2

--- src/page/user-login/index.js
+++ src/page/user-login/index.js
@@ -1,2 +1,72 @@
 require('./index.css');
-require('page/common/nav-simple/index.css')
+require('page/common/nav-simple/index.css');
+var _user = require('service/user-server');
+var _mm = require('util/mm');
+var fromError = {
+    //表单里的错误提示
+    show: function (errMsg) {
+        $('.error-item').show().find('.err-msg').text(errMsg);
+    },
+    hide: function () {
+        $('.error-item').hide().find('.err-msg').text('');
+    }
+};
+var page = {
+    init: function () {
+        this.bindEvent();
+    },
+    bindEvent: function () {
+        var _this = this;
+        //登录按钮的点击
+        $('#submit').click(function () {
+            _this.submit();
+        });
+        // 按下回车，也进行提交
+        $('.user-content').keyup(function (e) {
+            if (e.keyCode === 13) {
+                _this.submit();
+            }
+        });
+    },
+    // 提交表单
+    submit: function () {
+        var formData = {
+            username: $.trim($('#username').val()),
+            password: $.trim($('#password').val())
+        };
+        // 表单验证结果
+        validateResult = this.formValidate(formData);
+        if (validateResult.status) {
+            _user.login(formData, function (res) {
+                console.log(123123)
+                // window.location.href = _mm.getUrlParam('redirect') || './index.html';
+            }, function (errMsg) {
+                fromError.show(errMsg);
+            })
+        } else {
+            fromError.show(validateResult.msg);
+        }
+    },
+    // 表单验证的验证
+    formValidate: function (formData) {
+        var result = {
+            status: false,
+            msg: ''
+        };
+        if (!_mm.validate(formData.username, 'require')) {
+            result.msg = '用户名不能为空';
+            return result;
+        }
+        if (!_mm.validate(formData.password, 'require')) {
+            result.msg = '密码不能为空';
+            return result;
+        }
+        // 通过验证，返回正确提示
+        result.status = true;
+        result.msg = '验证通过';
+        return result;
+    }
+};
+$(function () {
+    page.init()
+});
