a689b8c74d43bd07ac745157ddcd46e94799fccc
ze25800000 <ze258100000@sina.com>
2017-11-11 07:50:51 +0800

    6-2 首页左侧关键字导航界面开发

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -1,6 +1,5 @@
 'use strict'
+require('./index.css')
 require('page/common/header/index')
 require('page/common/nav/index')
-var navSide = require('page/common/nav-side/index')
 var _mm = require('util/mm.js');
-navSide.init({name: 'user-center'});
