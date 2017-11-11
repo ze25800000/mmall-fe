d0d546da056aba2dbb403d8ac4b5b70745c9ae5e
ze25800000 <ze258100000@sina.com>
2017-11-11 10:25:48 +0800

    6-4 首页banner及楼层界面开发2

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -2,4 +2,19 @@
 require('./index.css')
 require('page/common/header/index')
 require('page/common/nav/index')
+require('util/slider/index')
+var templateBanner = require('./index.str')
 var _mm = require('util/mm.js');
+$(function () {
+    // 渲染banner的html
+    var bannerHtml = _mm.renderHtml(templateBanner);
+    $('.banner-con').html(bannerHtml);
+    $('.banner').unslider({
+        dots: true
+    });
+    //前一张和后一张事件操作
+    $('.banner-con .banner-arrow').click(function () {
+        var forward = $(this).hasClass('prev') ? 'prev' : 'next';
+        $('.slider').data('unslider')[forward]();
+    })
+});
