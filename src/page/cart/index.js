2b48c1ad8b890778fe5a9ace1404a7284c009f92
ze25800000 <ze258100000@sina.com>
2017-11-30 11:28:30 +0800

    7-5 购物车页面的实现（逻辑）2

--- src/page/cart/index.js
+++ src/page/cart/index.js
@@ -15,6 +15,82 @@ var page = {
         this.loadCart();
     },
     bindEvent: function () {
+        var _this = this;
+        $(document).on('click', '.cart-select', function () {
+            var $this = $(this),
+                productId = $this.parents('.cart-table').data('product-id');
+            // 切换选中状态
+            if ($this.is(':checked')) {
+                _cart.selectProduct(productId, function (res) {
+                    _this.renderCart(res);
+                }, function (errMsg) {
+                    _this.showCartError(errMsg);
+                });
+            } else {
+                _cart.unselectProduct(productId, function (res) {
+                    _this.renderCart(res);
+                }, function (errMsg) {
+                    _this.showCartError(errMsg);
+                });
+            }
+        });
+        // 全选/非全选
+        $(document).on('click', '.cart-select-all', function () {
+            var $this = $(this);
+            // 切换选中状态
+            if ($this.is(':checked')) {
+                _cart.selectAllProduct(function (res) {
+                    _this.renderCart(res);
+                }, function (errMsg) {
+                    _this.showCartError(errMsg);
+                });
+            } else {
+                _cart.unselectAllProduct(function (res) {
+                    _this.renderCart(res);
+                }, function (errMsg) {
+                    _this.showCartError(errMsg);
+                });
+            }
+        });
+        // 商品数量的变化
+        $(document).on('click', '.count-btn', function () {
+            var $this = $(this),
+                $pCount = $this.siblings('.count-input'),
+                type = $this.hasClass('plus') ? 'plus' : 'minus',
+                productId = $this.parents('.cart-table').data('product-id'),
+                currCount = parseInt($pCount.val()),
+                minCount = 1,
+                maxCount = parseInt($pCount.data('max')),
+                newCount = 0;
+            if (type === 'plus') {
+                if (currCount >= maxCount) {
+                    _mm.errorTips('该商品数量达到上限');
+                    return;
+                }
+                newCount = currCount + 1;
+            } else if (type === 'minus') {
+                if (currCount <= minCount) {
+                    return;
+                }
+                newCount = currCount - 1;
+            }
+            // 购物车商品数量
+            _cart.updateProduct({
+                productId: productId,
+                count: newCount
+            }, function (res) {
+                _this.renderCart(res);
+            }, function (errMsg) {
+                _this.showCartError(errMsg);
+            });
+        });
+        //删除单个商品
+        $(document).on('click', '.cart-delete', function () {
+            if (window.confirm('你确认要删除该商品？')) {
+                var productId = $(this).parents('.cart-table').data('product-id');
+                _this.delectCartProduct(productId);
+            }
+        })
     },
     // 加载购物车信息
     loadCart: function () {
@@ -23,12 +99,21 @@ var page = {
         _cart.getCartList(function (res) {
             _this.renderCart(res);
         }, function (errMsg) {
-            $('.page-wrap').html('<p class="err-tip">错误</p>')
+            _this.showCartError(errMsg);
         });
     },
     // 数据匹配
     filter: function (data) {
         data.notEmpty = !!data.cartProductVoList.length;
+    },
+    // 删除指定商品，支持批量，productId用逗号分隔
+    delectCartProduct: function (productIds) {
+        var _this = this;
+        _cart.deleteProduct(productIds, function (res) {
+            _this.renderCart(res);
+        }, function (errMsg) {
+            _this.showCartError(errMsg);
+        })
     },
     // 渲染购物车
     renderCart: function (data) {
@@ -38,6 +123,9 @@ var page = {
         // 生成HTML
         var cartHtml = _mm.renderHtml(templateIndex, data);
         $('.page-wrap').html(cartHtml);
+    },
+    showCartError: function (err) {
+        $('.page-wrap').html('<p class="err-tip">' + err + '</p>');
     }
 };
 $(function () {
--- src/service/cart-service.js
+++ src/service/cart-service.js
@@ -25,6 +25,64 @@ var _cart = {
             success: resolve,
             error: reject
         });
+    },
+    //选择购物车商品
+    selectProduct: function (productId, resolve, reject) {
+        _mm.request({
+            url: _mm.getServerUrl('/cart/select.do'),
+            data: {
+                productId: productId
+            },
+            success: resolve,
+            error: reject
+        });
+    },
+    //取消选择购物车商品
+    unselectProduct: function (productId, resolve, reject) {
+        _mm.request({
+            url: _mm.getServerUrl('/cart/un_select.do'),
+            data: {
+                productId: productId
+            },
+            success: resolve,
+            error: reject
+        });
+    },
+    //选择全部商品
+    selectAllProduct: function (resolve, reject) {
+        _mm.request({
+            url: _mm.getServerUrl('/cart/select_all.do'),
+            success: resolve,
+            error: reject
+        });
+    },
+    //取消选择全部商品
+    unselectAllProduct: function (resolve, reject) {
+        _mm.request({
+            url: _mm.getServerUrl('/cart/un_select_all.do'),
+            success: resolve,
+            error: reject
+        });
+    },
+    //更新购物车商品数量
+    updateProduct: function (productInfo, resolve, reject) {
+        _mm.request({
+            url: _mm.getServerUrl('/cart/update.do'),
+            data: productInfo,
+            success: resolve,
+            error: reject
+        });
+    },
+    //购物车删除商品
+    deleteProduct: function (productIds, resolve, reject) {
+        _mm.request({
+            url: _mm.getServerUrl('/cart/delete_product.do'),
+            data: {
+                productIds: productIds
+            },
+            success: resolve,
+            error: reject
+        });
     }
 }
 module.exports = _cart;
