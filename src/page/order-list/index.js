e1a465987b4039ab6f739466852017906ab5b420
ze25800000 <ze258100000@sina.com>
2017-12-08 09:19:22 +0800

    8-10 订单列表的开发（逻辑）

--- src/page/order-list/index.js
+++ src/page/order-list/index.js
@@ -3,17 +3,58 @@ require('page/common/header/index')
 require('page/common/nav/index')
 var navSide = require('page/common/nav-side/index')
 var _mm = require('util/mm.js');
-var _user = require('service/user-service')
+var _order = require('service/order-service')
 var templateIndex = require('./index.str')
+var Pagination = require('util/pagination/index.js');
 var page = {
+    data: {
+        listParam: {
+            pageNum: 1,
+            pageSize: 10
+        }
+    },
     init: function () {
         this.onLoad();
     },
     onLoad: function () {
+        this.loadOrderList();
         //初始化左侧菜单
         navSide.init({
             name: 'order-list'
         });
+    },
+    // 加载订单列表
+    loadOrderList: function () {
+        var orderListHtml = '',
+            _this = this,
+            $listCon = $('.order-list-con');
+        $listCon.html('<div class="loading"></div>');
+        _order.getOrderList(this.data.listParam, function (res) {
+            orderListHtml = _mm.renderHtml(templateIndex, res);
+            $listCon.html(orderListHtml);
+            _this.loadPagination({
+                hasPreviousPage: res.hasPreviousPage,
+                prePage: res.prePage,
+                hasNextPage: res.hasNextPage,
+                nextPage: res.nextPage,
+                pageNum: res.pageNum,
+                pages: res.pages
+            });
+        }, function (errMsg) {
+            $listCon.html('<p class="err-tip">加载订单失败，请刷新后重试</p>')
+        })
+    },
+    // 加载分页信息
+    loadPagination: function (pageInfo) {
+        var _this = this;
+        this.pagination ? '' : (this.pagination = new Pagination());
+        this.pagination.render($.extend({}, pageInfo, {
+            container: $('.pagination'),
+            onSelectPage: function (pageNum) {
+                _this.data.listParam.pageNum = pageNum;
+                _this.loadOrderList();
+            }
+        }));
     }
 };
 $(function () {
--- src/service/order-service.js
+++ src/service/order-service.js
@@ -20,6 +20,15 @@ var _order = {
             success: resolve,
             error: reject
         });
+    },
+    // 获取订单列表
+    getOrderList: function (listParam, resolve, reject) {
+        _mm.request({
+            url: _mm.getServerUrl('/order/list.do'),
+            data: listParam,
+            success: resolve,
+            error: reject
+        });
     }
 };
 module.exports = _order;
